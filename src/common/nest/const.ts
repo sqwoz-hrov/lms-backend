3236812e440e689bbbfa531699ee37f42f62b303
Goldcat <goldcvt.develops@gmail.com>
2025-05-30 19:42:43 +0300

    feat: improve API documentation tooling

---
+++ src/common/nest/const.ts
@@ -0,0 +1,1 @@
+export const DEFAULT_ERROR_MESSAGE = 'Что-то пошло не так на нашей стороне :(';
--- src/common/nest/decorators/route.decorator.ts
+++ src/common/nest/decorators/route.decorator.ts
@@ -2,17 +2,25 @@ import { Type, UseFilters, UseInterceptors, applyDecorators } from '@nestjs/comm
 import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AllExceptionsFilter } from '../exception-filters/all-exceptions.filter';
 import { RequestLoggerInterceptor } from '../interceptors/request-logger.interceptor';
+// This is a hack but it helps with typing
+import type { SchemaObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
 
 export const Route = ({
 	summary,
 	description,
 	responseType,
 	isArray,
+	possibleErrors,
 }: {
 	summary: string;
 	description?: string;
 	responseType?: Type<unknown>;
 	isArray?: boolean;
+	possibleErrors?: {
+		status?: number;
+		description?: string;
+		schema?: SchemaObject;
+	}[];
 }) =>
 	applyDecorators(
 		UseFilters(AllExceptionsFilter),
@@ -25,4 +33,17 @@ export const Route = ({
 			type: responseType,
 			isArray: isArray ?? false,
 		}),
+		...(possibleErrors?.map(type =>
+			ApiResponse({
+				status: type.status ?? 500,
+				description: type.description ?? 'Ошибка',
+				...(type.schema && {
+					content: {
+						'application/json': {
+							schema: type.schema,
+						},
+					},
+				}),
+			}),
+		) ?? []),
 	);
