935d4f15dfa8fb354103e5e68156d96ff4507f26
Artem Zeinalov <zeynchan@gmail.com>
2025-11-17 13:36:53 +0100

    fix: proper defaults in config

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -7,7 +7,7 @@ export const redisConfig = registerAs('redis', () => ({
 	redisUsername: get('REDIS_USERNAME').asString(),
 	redisPassword: get('REDIS_PASSWORD').required().asString(),
 	redisLazyConnect: get('REDIS_LAZY_CONNECT').default('false').asBool(),
-	redisTlsCa: get('REDIS_TLS_CA').default('').asString(),
-	redisTlsCert: get('REDIS_TLS_CERT').default('').asString(),
-	redisTlsKey: get('REDIS_TLS_KEY').default('').asString(),
+	redisTlsCa: get('REDIS_TLS_CA').default('/certs/ca.crt').asString(),
+	redisTlsCert: get('REDIS_TLS_CERT').default('/certs/publisher.crt').asString(),
+	redisTlsKey: get('REDIS_TLS_KEY').default('/certs/publisher.crt').asString(),
 }));
