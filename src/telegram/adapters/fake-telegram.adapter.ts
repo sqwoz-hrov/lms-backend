37e9b97d02f0cd46a303429c0cf56f6361c6b52a
goldcvt <thegoldcat5@gmail.com>
2026-01-28 01:59:35 +0300

    feat: use local OTP instead of telegram in DEV environment

--- src/telegram/adapters/fake-telegram.adapter.ts
+++ src/telegram/adapters/fake-telegram.adapter.ts
@@ -1,11 +1,15 @@
+import { Logger } from '@nestjs/common';
+import { OTP } from '../../user/core/otp';
 import { IOTPSender } from '../../user/ports/otp-sender.port';
 
 export class FakeTelegramAdapter implements IOTPSender<'telegram'> {
+	private readonly logger = new Logger(FakeTelegramAdapter.name);
 	public sendMessage(): Promise<boolean> {
 		return Promise.resolve(true);
 	}
 
-	public sendOTP(): Promise<boolean> {
+	public sendOTP(to: { telegram_id: number }, otp: OTP): Promise<boolean> {
+		this.logger.log(`Sending OTP ${otp.asString} to Telegram user ${to.telegram_id}`);
 		return Promise.resolve(true);
 	}
 }
